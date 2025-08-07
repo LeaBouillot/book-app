4f45ca1360a0489e4e48974bee970c34f61c0533
bestdevelope <leabouillot7@gmail.com>
2025-08-07 19:45:39 +0200

    layout-navbar

--- book-app/src/pages/Home.jsx
+++ book-app/src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      
+      <h1>Accueil - Liste des Livres</h1>
+      <Link to="/add">Ajouter un livre</Link>
+      {/* Ici, vous pouvez afficher la liste des livres */}
     </div>
-  )
+  );
 }
 
 export default Home
